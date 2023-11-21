a0f3345b54120c43b48e669e685543b01cfb38ee
MTulioDSpirito <mtdenucci@gmail.com>
2023-11-21 19:21:31 -0300

    update telas atividades

--- src/pages/pontuação_tela11/pontuacao.js
+++ src/pages/pontuação_tela11/pontuacao.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
   var usuario = JSON.parse(localStorage.getItem('usuario'));
   if(usuario) {
@@ -7,4 +8,21 @@ window.onload = function() {
   let points = Number(localStorage.getItem('points')) || 0;
   // Exiba a pontuação total na tela.
   document.getElementById('score').innerText = points;
+  // Altere o tamanho e a cor do texto com base na pontuação.
+  let scoreElement = document.getElementById('score');
+  let messageElement = document.getElementById('message');
+  scoreElement.style.fontSize = '2em';
+  if (points <= 3) {
+    scoreElement.style.color = 'red';
+    messageElement.style.color = 'red';
+    messageElement.innerText = 'Infelizmente não foi dessa vez, clique abaixo e recomece novamente';
+  } else if (points <= 6) {
+    scoreElement.style.color = 'darkorange';
+    messageElement.style.color = 'darkorange';
+    messageElement.innerText = 'Ótimo trabalho';
+  } else if (points == 9) {
+    scoreElement.style.color = 'green';
+    messageElement.style.color = 'green';
+    messageElement.innerText = 'Parabéns, você brilhou';
+  }
 }
