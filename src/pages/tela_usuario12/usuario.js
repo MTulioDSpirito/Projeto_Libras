21ed859ba631eba10048d9d90f37c81cbd85f63f
Marcos S. M. Sousa <112666835+marcosabino-pucmg@users.noreply.github.com>
2023-11-27 21:20:51 -0300

    att

--- src/pages/tela_usuario12/usuario.js
+++ src/pages/tela_usuario12/usuario.js
@@ -1,4 +1,20 @@
+function prencherEmail () {
+    var usuario = JSON.parse (localStorage.getItem("usuario"));
 
 
+    if (usuario) {
+        document.getElementById ("emailInput").value = usuario.email;
+    }
 
+    if (usuario) {
+        document.getElementById ("user-info").textContent = usuario.nome;
+    }
+
+    if (usuario) {
+        document.getElementById ("passwordInput").value = usuario.senha;
+    }
+
+}
+
+window.onload = prencherEmail; 
 
