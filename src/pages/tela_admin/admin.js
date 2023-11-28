c4f49e1a551146ae6ad875eee3e31e85012eb571
Marcos S. M. Sousa <112666835+marcosabino-pucmg@users.noreply.github.com>
2023-11-28 20:29:40 -0300

    a

--- src/pages/tela_admin/admin.js
+++ src/pages/tela_admin/admin.js
@@ -1,14 +1,14 @@
-// Função para verificar o LocalStorage e criar a lista para a chave "usuario"
+
 function criarListaUsuarioLocalStorage() {
     // Verifique se há dados no localStorage
     if (localStorage.length > 0) {
-      // Recupere o valor associado à chave "usuario" no localStorage
+
       const usuario = localStorage.getItem('usuario');
 
-      // Elemento ul onde a lista será exibida
+
       const listaLocalStorage = document.getElementById('listaLocalStorage');
 
-      // Se a chave "usuario" existir, crie um elemento de lista para ela
+
       if (usuario !== null) {
         const listItem = document.createElement('li');
         listItem.textContent = `usuario: ${usuario}`;
@@ -21,5 +21,4 @@ function criarListaUsuarioLocalStorage() {
     }
   }
 
-  // Chame a função para criar a lista quando a página carregar
   window.onload = criarListaUsuarioLocalStorage;
