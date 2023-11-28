effaf2fea59e06492a00c43f453daef8601f9073
Marcos S. M. Sousa <112666835+marcosabino-pucmg@users.noreply.github.com>
2023-11-28 20:34:09 -0300

    atualizado adm js

--- src/pages/tela_admin/admin.js
+++ src/pages/tela_admin/admin.js
@@ -1,24 +1,20 @@
-
-function criarListaUsuarioLocalStorage() {
-    // Verifique se há dados no localStorage
-    if (localStorage.length > 0) {
-
-      const usuario = localStorage.getItem('usuario');
-
-
-      const listaLocalStorage = document.getElementById('listaLocalStorage');
-
-
-      if (usuario !== null) {
-        const listItem = document.createElement('li');
-        listItem.textContent = `usuario: ${usuario}`;
-        listaLocalStorage.appendChild(listItem);
-      } else {
-        console.log('Chave "usuario" não encontrada no localStorage.');
+   function criarListaNomeEmailUsuarioLocalStorage() {
+        if (localStorage.length > 0) {
+          const usuarioString = localStorage.getItem('usuario');
+          const usuarioObjeto = JSON.parse(usuarioString);
+          if (usuarioObjeto && usuarioObjeto.nome && usuarioObjeto.email) {
+            const listaLocalStorage = document.getElementById('listaLocalStorage');
+            const listItemNome = document.createElement('li');
+            listItemNome.textContent = `nome: ${usuarioObjeto.nome}`;
+            listaLocalStorage.appendChild(listItemNome);
+            const listItemEmail = document.createElement('li');
+            listItemEmail.textContent = `email: ${usuarioObjeto.email}`;
+            listaLocalStorage.appendChild(listItemEmail);
+          } else {
+            console.log('Chave "usuario" e/ou subchaves "nome" e "email" não encontradas no localStorage.');
+          }
+        } else {
+          console.log('Nenhum dado encontrado no localStorage.');
+        }
       }
-    } else {
-      console.log('Nenhum dado encontrado no localStorage.');
-    }
-  }
-
-  window.onload = criarListaUsuarioLocalStorage;
+      window.onload = criarListaNomeEmailUsuarioLocalStorage;
