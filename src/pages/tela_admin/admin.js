6f0fdf9d530f1070eac95bce09c7c9c86e50e3a5
Marcos S. M. Sousa <112666835+marcosabino-pucmg@users.noreply.github.com>
2023-11-28 20:22:42 -0300

    atualização adm

--- src/pages/tela_admin/admin.js
+++ src/pages/tela_admin/admin.js
@@ -1,24 +1,25 @@
+// Função para verificar o LocalStorage e criar a lista para a chave "usuario"
+function criarListaUsuarioLocalStorage() {
+    // Verifique se há dados no localStorage
+    if (localStorage.length > 0) {
+      // Recupere o valor associado à chave "usuario" no localStorage
+      const usuario = localStorage.getItem('usuario');
 
-        // Função para verificar o LocalStorage e criar a lista
-        function criarListaLocalStorage() {
-          // Verifique se há dados no localStorage
-          if (localStorage.length > 0) {
-            // Recupere os dados do localStorage
-            const dadosLocalStorage = Object.entries(localStorage);
-    
-            // Elemento ul onde a lista será exibida
-            const listaLocalStorage = document.getElementById('listaLocalStorage');
-    
-            // Itere sobre os dados e crie elementos li para cada um
-            dadosLocalStorage.forEach(([chave, valor]) => {
-              const listItem = document.createElement('li');
-              listItem.textContent = `${chave}: ${valor}`;
-              listaLocalStorage.appendChild(listItem);
-            });
-          } else {
-            console.log('Nenhum dado encontrado no localStorage.');
-          }
-        }
-    
-        // Chame a função para criar a lista quando a página carregar
-        window.onload = criarListaLocalStorage;
+      // Elemento ul onde a lista será exibida
+      const listaLocalStorage = document.getElementById('listaLocalStorage');
+
+      // Se a chave "usuario" existir, crie um elemento de lista para ela
+      if (usuario !== null) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `usuario: ${usuario}`;
+        listaLocalStorage.appendChild(listItem);
+      } else {
+        console.log('Chave "usuario" não encontrada no localStorage.');
+      }
+    } else {
+      console.log('Nenhum dado encontrado no localStorage.');
+    }
+  }
+
+  // Chame a função para criar a lista quando a página carregar
+  window.onload = criarListaUsuarioLocalStorage;
