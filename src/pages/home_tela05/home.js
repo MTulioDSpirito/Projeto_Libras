4b513b216616332be62f31097808206cf6d67761
Marcos S. M. Sousa <112666835+marcosabino-pucmg@users.noreply.github.com>
2023-11-30 19:38:13 -0300

    inserção do botão logout tela home

--- src/pages/home_tela05/home.js
+++ src/pages/home_tela05/home.js
@@ -4,3 +4,8 @@ window.onload = function() {
     document.getElementById('user-info').textContent = usuario.nome;
   }
 }
+
+document.getElementById('logoutBtn').addEventListener('click', function() {
+  alert("Logout realizado com sucesso!");
+  window.location.href = "../login_tela3/login.html";
+});
