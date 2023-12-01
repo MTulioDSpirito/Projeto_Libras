e71d78a1bbbaa31a4401ad592e42521b1dc49f73
Felpa147 <fellipe.rodrigues@sga.pucminas.br>
2023-12-01 04:58:23 -0300

    Exercícios avançados e releases menores

---
+++ src/pages/avancado3/atividade_3.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+    if (usuario) {
+        document.getElementById('user-info').textContent = usuario.nome;
+    }
+}
+
+function checkAnswer() {
+    const userInput = document.getElementById('user-input').value.toLowerCase();
+
+    // Verifique se o usuário já respondeu à pergunta anteriormente
+    if (localStorage.getItem('11')) {
+        alert('Você já acertou esta pergunta antes. Não serão atribuídos mais pontos.');
+        return; // Saia da função para evitar atribuir mais pontos
+    }
+
+    let points = Number(localStorage.getItem('points')) || 0;
+
+    // Adapte a lógica para pontuar com base na resposta do usuário em texto
+    if (userInput === 'borboleta') {
+        points += 3;
+
+        // Marque a pergunta como acertada para evitar atribuir mais pontos
+        localStorage.setItem('11', true);
+    }
+
+    localStorage.setItem('points', points);
+
+    // Adicione aqui a lógica para avançar para a próxima atividade ou página
+    // Por exemplo, redirecionar para a próxima página de atividade
+    // window.location.href = 'próxima_pagina.html'; // Substitua 'próxima_pagina.html' pelo seu URL desejado
+}
