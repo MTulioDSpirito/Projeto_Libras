a0ba05075bb1a59b8c11e42009ecc326f42da637
MTulioDSpirito <mtdenucci@gmail.com>
2023-12-04 21:42:01 -0300

    fix screen adm

--- src/pages/tela_admin/admin.js
+++ src/pages/tela_admin/admin.js
@@ -1,45 +1,56 @@
 function criarListaNomeEmailUsuarioLocalStorage() {
-  if (localStorage.length > 0) {
-      const usuariosString = localStorage.getItem('usuarios');
-      const usuariosArray = JSON.parse(usuariosString);
-      
-      if (usuariosArray && usuariosArray.length > 0) {
-          const listaLocalStorage = document.getElementById('listaLocalStorage');
-          
-          for (let i = 0; i < usuariosArray.length; i++) {
-              const usuario = usuariosArray[i];
-              
-              if (usuario && usuario.nome && usuario.email) {
-                  const listItemNome = document.createElement('li');
-                  listItemNome.textContent = `Nome: ${usuario.nome}`;
-                  listaLocalStorage.appendChild(listItemNome);
-                  
-                  const listItemEmail = document.createElement('li');
-                  listItemEmail.textContent = `Email: ${usuario.email}`;
-                  listaLocalStorage.appendChild(listItemEmail);
-
-                  // Adicionar uma quebra de linha após cada par de nome e e-mail
-                  const breakLine = document.createElement('br');
-                  listaLocalStorage.appendChild(breakLine);
-              }
-          }
-      } else {
-          console.log('Chave "usuarios" não encontrada no localStorage ou nenhum usuário salvo.');
-      }
-  } else {
-      console.log('Nenhum dado encontrado no localStorage.');
-  }
-}
-
-window.onload = criarListaNomeEmailUsuarioLocalStorage;
-
-
-
-document.getElementById('erase').addEventListener('click', function() {
-    // Limpar todos os usuários do localStorage
-    localStorage.removeItem('usuarios');
+    if (localStorage.length > 0) {
+        const usuariosString = localStorage.getItem('usuarios');
+        const usuariosArray = JSON.parse(usuariosString);
+        
+        if (usuariosArray && usuariosArray.length > 0) {
+            const listaLocalStorage = document.getElementById('listaLocalStorage');
+            
+            for (let i = 0; i < usuariosArray.length; i++) {
+                const usuario = usuariosArray[i];
+                
+                if (usuario && usuario.nome && usuario.email) {
+                    // Criar um div para cada usuário
+                    const userDiv = document.createElement('div');
+                    userDiv.style.display = 'flex';
+                    userDiv.style.alignItems = 'center';
+                    listaLocalStorage.appendChild(userDiv);
   
-    // Limpar a lista de usuários na página
-    document.getElementById('listaLocalStorage').innerHTML = '';
-  });
+                    // Criar um botão de apagar para cada usuário
+                    const eraseButton = document.createElement('button');
+                    eraseButton.textContent = 'Apagar';
+                    eraseButton.addEventListener('click', function() {
+                        // Remover o usuário do array
+                        usuariosArray.splice(i, 1);
+                        
+                        // Salvar o array atualizado no localStorage
+                        localStorage.setItem('usuarios', JSON.stringify(usuariosArray));
+                        
+                        // Remover o div do usuário da página
+                        listaLocalStorage.removeChild(userDiv);
+                    });
+                    userDiv.appendChild(eraseButton);
+  
+                    const listItemNome = document.createElement('li');
+                    listItemNome.textContent = `Nome: ${usuario.nome}`;
+                    userDiv.appendChild(listItemNome);
+                    
+                    const listItemEmail = document.createElement('li');
+                    listItemEmail.textContent = `Email: ${usuario.email}`;
+                    userDiv.appendChild(listItemEmail);
+    
+                    // Adicionar uma quebra de linha após cada usuário
+                    const breakLine = document.createElement('br');
+                    listaLocalStorage.appendChild(breakLine);
+                }
+            }
+        } else {
+            console.log('Chave "usuarios" não encontrada no localStorage ou nenhum usuário salvo.');
+        }
+    } else {
+        console.log('Nenhum dado encontrado no localStorage.');
+    }
+  }
+  
+  window.onload = criarListaNomeEmailUsuarioLocalStorage;
   
